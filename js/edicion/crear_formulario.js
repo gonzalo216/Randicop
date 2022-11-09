6d8d3a9dd6c2687364dd1895d7104a9b91bcfcdd
Juerte99 <86316267+Juerte99@users.noreply.github.com>
2022-11-08 22:53:16 -0300

    eliminar cheto

--- js/edicion/crear_formulario.js
+++ js/edicion/crear_formulario.js
@@ -1,51 +1,62 @@
+import { json } from "/json/nombres.js";
+
 const d = document;
-export function crearFormulario(num, eliminar = false) {
+
+export function getJson(num) {
   const $template = d.querySelector("template").content,
     $fragment = d.createDocumentFragment();
-  fetch("/json/nombres.json")
-    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-    .then((json) => {
-      const $ids = d.querySelectorAll(`input[name="id"]`);
-      let lastId;
-      if ($ids.length !== 0) {
-        lastId = $ids[$ids.length - 1].value;
-        lastId = parseInt(lastId, 10);
-        if (eliminar) {
-          const $divs = d.querySelectorAll(".celda");
-          if ($divs.length > 1) {
-            $divs[$divs.length - 1].remove();
-            lastId--;
-          }
-          return;
-        }
-      } else {
-        lastId = 0;
-      }
-      num += lastId;
-      for (let i = lastId; i < num; i++) {
-        if (!json[i]) return;
-        const el = json[i];
-        $template.querySelector(`input[name="nombre"]`).value = el.name;
-        $template.querySelector(`input[name="id"]`).value = el.id;
+  for (let i = 0; i < num; i++) {
+    if (!json[i]) return;
+    const el = json[i];
+    $template.querySelector(`input[name="nombre"]`).value = el.name;
+    $template.querySelector(`button[name="id"]`).dataset.id = i;
 
-        let $clone = d.importNode($template, true);
-        $fragment.appendChild($clone);
-      }
-      d.querySelector(".flex").appendChild($fragment);
-    })
-    .catch((error) => console.error(error));
+    let $clone = d.importNode($template, true);
+    $fragment.appendChild($clone);
+  }
+  d.querySelector(".flex").appendChild($fragment);
+}
+export function putJson() {
+  console.log(d.querySelectorAll(`button[name="id"]`));
+  const $template = d.querySelector("template").content,
+    $fragment = d.createDocumentFragment(),
+    $ids = [];
+  d.querySelectorAll(`button[name="id"]`).forEach((el) =>
+    $ids.push(el.dataset.id)
+  );
+  let i = Object.keys(json).findIndex((el) => {
+    if (!$ids.includes(el)) return el;
+  });
+  if (i === -1) return;
+
+  const el = json[i];
+  $template.querySelector(`input[name="nombre"]`).value = el.name;
+  $template.querySelector(`button[name="id"]`).dataset.id = i;
+
+  let $clone = d.importNode($template, true);
+  $fragment.appendChild($clone);
+  d.querySelector(".flex").appendChild($fragment);
+}
+export function deleteJson(target) {
+  const $celdas = d.querySelectorAll(".celda");
+  if ($celdas.length !== 0) {
+    if (!target) {
+      $celdas[$celdas.length - 1].remove();
+      return;
+    }
+    $celdas.forEach((el) => {
+      if (
+        el.querySelector(`button[name="id"]`).dataset.id === target.dataset.id
+      )
+        el.remove();
+    });
+  }
 }
 export function updateJson() {
   const $celdas = d.querySelectorAll(".celda");
-  $celdas.forEach(($celda, i) => {
-    fetch(`/json/nombres.json/${i + 1}`)
-      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-      .then((json) => {
-        if (!json[i + 1]) return;
-        const el = json[i + 1];
-        el.name = $celda.querySelector(`input[name="nombre"]`).value;
-        el.id = $celda.querySelector(`input[name="id"]`).value;
-      })
-      .catch((error) => console.error(error));
-  });
+  $celdas.forEach(
+    ($celda) =>
+      (json[$celda.querySelector(`button[name="id"]`)].name =
+        $celda.querySelector(`input[name="nombre"]`).value)
+  );
 }
--- js/index_edicion.js
+++ js/index_edicion.js
@@ -1,17 +1,15 @@
-import { crearFormulario } from "./edicion/crear_formulario.js";
+import { deleteJson, getJson, putJson } from "./edicion/crear_formulario.js";
 import { toggleDescription } from "./edicion/toggle_description.js";
 
 const d = document;
 export function iniciarpre() {
-  crearFormulario(5);
+  getJson(5);
 }
 d.addEventListener("click", (e) => {
-  if (e.target.matches("#mas") || e.target.matches("#mas *")) {
-    crearFormulario(1);
-  }
-  if (e.target.matches("#menos") || e.target.matches("#menos *")) {
-    crearFormulario(0, true);
-  }
+  if (e.target.matches("#mas") || e.target.matches("#mas *")) putJson();
+  if (e.target.matches("#menos") || e.target.matches("#menos *"))
+    deleteJson(false);
+  if (e.target.matches(`button[name="id"]`)) deleteJson(e.target);
   if (e.target.matches("summary") || e.target.matches("summary *")) {
     toggleDescription();
   }
