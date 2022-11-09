445965d84834caec6f0617491db128e3a817c4eb
Melina Troncoso <ignaciotroche12@gmail.com>
2022-11-09 11:58:53 -0300

    minecraft en el header

--- js/header/header.js
+++ js/header/header.js
@@ -1,6 +1,7 @@
 const d = document,
   $header = d.querySelector(".header"),
   $title = d.querySelector("h1"),
+  $minei = d.querySelector("h2"),
   $logo = d.querySelector(".logo-link"),
   $button = d.querySelector(".panel-btn");
 let topAnterior = true;
@@ -11,7 +12,9 @@ export default function scaleHeader() {
     topAnterior = top;
     $header.classList.toggle("sticky");
     $title.classList.toggle("sticky");
+    $minei.classList.toggle("sticky");
     $logo.classList.toggle("sticky");
     $button.classList.toggle("sticky");
+
   }
 }
